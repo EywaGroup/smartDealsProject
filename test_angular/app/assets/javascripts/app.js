a63dad22463f5fe6b688b6dbcb18c46d47b33633
IlgizZ <ilgiz1996@mail.ru>
2016-11-18 21:34:21 +0300

    Revert "add id and image url"
    This reverts commit 77084ee4102d8586867a015c4b7087969433513c.

--- test_angular/app/assets/javascripts/app.js
+++ test_angular/app/assets/javascripts/app.js
@@ -7,17 +7,17 @@ app.config(function($mdThemingProvider) {
   $mdThemingProvider.theme('dark-purple').backgroundPalette('deep-purple').dark();
   $mdThemingProvider.theme('dark-blue').backgroundPalette('blue').dark();
 });
-
 app.controller('smartDealsController',function($scope,$mdDialog){
   var web3 = new Web3();
   this.pizzas = pizzas;
   var variable;
 
-  $scope.showAdvanced = function(ev) {
 
-    $mdDialog.show({
-      controller: function DialogController($scope, $mdDialog) {$scope.hide = function() {
-        $mdDialog.hide();
+  $scope.showAdvanced = function(ev) {
+      $mdDialog.show({
+        controller: function DialogController($scope, $mdDialog) {
+    $scope.hide = function() {
+      $mdDialog.hide();
     };
 
     $scope.cancel = function() {
@@ -28,18 +28,18 @@ app.controller('smartDealsController',function($scope,$mdDialog){
       $mdDialog.hide(answer);
     };
   },
-    templateUrl: 'solution_controller/index',
-    parent: angular.element(document.body),
-    targetEvent: ev,
-    clickOutsideToClose:true,
-    fullscreen: $scope.customFullscreen // Only for -xs, -sm breakpoints.
-  })
-  .then(function(answer) {
-    $scope.status = 'You said the information was "' + answer + '".';
-  }, function() {
-    $scope.status = 'You cancelled the dialog.';
-  });
-};
+        templateUrl: 'solution_controller/index',
+        parent: angular.element(document.body),
+        targetEvent: ev,
+        clickOutsideToClose:true,
+        fullscreen: $scope.customFullscreen // Only for -xs, -sm breakpoints.
+      })
+      .then(function(answer) {
+        $scope.status = 'You said the information was "' + answer + '".';
+      }, function() {
+        $scope.status = 'You cancelled the dialog.';
+      });
+    };
 
 
 
@@ -50,22 +50,33 @@ app.controller('smartDealsController',function($scope,$mdDialog){
         price: 400,
         description: 'Very good pizza!',
         id: 1,
-        image: 'assets/6.jpg',
-        
+        images: [
+          "assets/images/1.png",
+          "assets/images/1.png",
+          "assets/images/1.png"
+        ],
       },
       {
         name: 'Quattro Formaggi',
         price: 300,
         description: 'Very good pizza!',
         id: 2,
-        image: 'assets/6.jpg',
+        images: [
+          "assets/images/1.png",
+          "assets/images/1.png",
+          "assets/images/1.png"
+        ],
       },
       {
         name: 'Maltija (Maltese)',
         price: 300,
         description: 'Very good pizza!',
         id: 3,
-        image: 'assets/6.jpg',
+        images: [
+          "assets/images/1.jpg",
+          "assets/images/1.jpg",
+          "assets/images/1.jpg"
+        ],
       }
     ];
 })();
