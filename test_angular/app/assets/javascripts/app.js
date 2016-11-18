3d4181019a687882f7b06363ec221f1aeb8b3cbb
IlgizZ <ilgiz1996@mail.ru>
2016-11-18 22:19:29 +0300

    roma bilyat' otkatil eto

--- test_angular/app/assets/javascripts/app.js
+++ test_angular/app/assets/javascripts/app.js
@@ -7,17 +7,17 @@ app.config(function($mdThemingProvider) {
   $mdThemingProvider.theme('dark-purple').backgroundPalette('deep-purple').dark();
   $mdThemingProvider.theme('dark-blue').backgroundPalette('blue').dark();
 });
+
 app.controller('smartDealsController',function($scope,$mdDialog){
   var web3 = new Web3();
   this.pizzas = pizzas;
   var variable;
 
-
   $scope.showAdvanced = function(ev) {
-      $mdDialog.show({
-        controller: function DialogController($scope, $mdDialog) {
-    $scope.hide = function() {
-      $mdDialog.hide();
+
+    $mdDialog.show({
+      controller: function DialogController($scope, $mdDialog) {$scope.hide = function() {
+        $mdDialog.hide();
     };
 
     $scope.cancel = function() {
@@ -28,18 +28,18 @@ app.controller('smartDealsController',function($scope,$mdDialog){
       $mdDialog.hide(answer);
     };
   },
-        templateUrl: 'solution_controller/index',
-        parent: angular.element(document.body),
-        targetEvent: ev,
-        clickOutsideToClose:true,
-        fullscreen: $scope.customFullscreen // Only for -xs, -sm breakpoints.
-      })
-      .then(function(answer) {
-        $scope.status = 'You said the information was "' + answer + '".';
-      }, function() {
-        $scope.status = 'You cancelled the dialog.';
-      });
-    };
+    templateUrl: 'solution_controller/index',
+    parent: angular.element(document.body),
+    targetEvent: ev,
+    clickOutsideToClose:true,
+    fullscreen: $scope.customFullscreen // Only for -xs, -sm breakpoints.
+  })
+  .then(function(answer) {
+    $scope.status = 'You said the information was "' + answer + '".';
+  }, function() {
+    $scope.status = 'You cancelled the dialog.';
+  });
+};
 
 
 
@@ -50,33 +50,22 @@ app.controller('smartDealsController',function($scope,$mdDialog){
         price: 400,
         description: 'Very good pizza!',
         id: 1,
-        images: [
-          "assets/images/1.png",
-          "assets/images/1.png",
-          "assets/images/1.png"
-        ],
+        image: 'assets/6.jpg',
+        
       },
       {
         name: 'Quattro Formaggi',
         price: 300,
         description: 'Very good pizza!',
         id: 2,
-        images: [
-          "assets/images/1.png",
-          "assets/images/1.png",
-          "assets/images/1.png"
-        ],
+        image: 'assets/6.jpg',
       },
       {
         name: 'Maltija (Maltese)',
         price: 300,
         description: 'Very good pizza!',
         id: 3,
-        images: [
-          "assets/images/1.jpg",
-          "assets/images/1.jpg",
-          "assets/images/1.jpg"
-        ],
+        image: 'assets/6.jpg',
       }
     ];
 })();
