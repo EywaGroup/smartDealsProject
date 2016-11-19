e5c4bcdac170703f295ccbb708ba57c7bfa8d2f2
Roman Anderson <greenclawanderson@gmail.com>
2016-11-19 08:15:35 +0300

    Fix

--- test_angular/app/assets/javascripts/app.js
+++ test_angular/app/assets/javascripts/app.js
@@ -15,7 +15,68 @@ app.controller('smartDealsController',function($scope,$mdDialog){
 //geth --rpc --rpcapi admin,eth,personal,miner  --testnet --rpccorsdomain="*" console
 //----------------------------------------------------------------------------------------
     
-     var abi =  [{"constant":false,"inputs":[{"name":"amount","type":"uint256"},{"name":"addr","type":"address"},{"name":"goods","type":"uint256"}],"name":"pay","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"contractDone","outputs":[],"payable":true,"type":"function"},{"constant":false,"inputs":[],"name":"contractNotDone","outputs":[],"payable":true,"type":"function"},{"constant":false,"inputs":[{"name":"tmpAddr","type":"address"}],"name":"search","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"provAddrs","type":"address[]"},{"name":"provCount","type":"uint256"},{"name":"mainAcc","type":"address"}],"name":"init","outputs":[],"payable":false,"type":"function"},{"anonymous":false,"inputs":[{"indexed":false,"name":"sender","type":"address"},{"indexed":false,"name":"_provAddress","type":"address"},{"indexed":false,"name":"_value","type":"uint256"}],"name":"Deposit","type":"event"}];
+     var abi =  [
+     {"constant":false,
+      "inputs":
+        [{"name":"amount","type":"uint256"},
+         {"name":"addr","type":"address"},
+         {"name":"goods","type":"uint256"}],
+         "name":"pay",
+         "outputs":[],
+         "payable":false,
+         "type":"function"},
+
+         {"constant":false,
+         "inputs":[],
+         "name":"contractDone",
+         "outputs":[],
+         "payable":true,
+         "type":"function"},
+
+         {"constant":false,
+         "inputs":[],
+         "name":"contractNotDone",
+         "outputs":[],
+         "payable":true,
+         "type":"function"},
+
+         {"constant":false,
+         "inputs":
+         [{"name":"tmpAddr",
+           "type":"address"}],
+           "name":"search",
+           "outputs":
+           [{"name":"",
+           "type":"uint256"}],
+           "payable":false,
+           "type":"function"},
+
+         {"constant":false,
+           "inputs":
+           [{"name":"provAddrs",
+           "type":"address[]"},
+           {"name":"provCount",
+           "type":"uint256"},
+           {"name":"mainAcc",
+           "type":"address"}],
+           "name":"init",
+           "outputs":[],
+           "payable":false,
+           "type":"function"},
+
+          {"anonymous":false,
+           "inputs":
+           [{"indexed":false,
+           "name":"sender",
+           "type":"address"},
+            {"indexed":false,
+            "name":"_provAddress",
+            "type":"address"},
+            {"indexed":false,
+            "name":"_value",
+            "type":"uint256"}],
+           "name":"Deposit",
+           "type":"event"}];
      var Web3 = require('web3');
      var web3 = new Web3();
      var eth = web3.eth;
@@ -36,16 +97,59 @@ app.controller('smartDealsController',function($scope,$mdDialog){
      $scope.myFunds = web3.fromWei(eth.getBalance(eth.coinbase),"ether");
      web3.personal.unlockAccount($scope.myToken,"Dtrnjh2917");
 
-     var source = "" + 
-    "contract test {\n" +
-    "   function multiply(uint a) returns(uint d) {\n" +
-    "       return a * 7;\n" +
-    "   }\n" +
-    "}\n";
+     var source = "pragma solidity ^0.4.0;\n"+ 
+"contract SmartDeal {\n" +
+"struct Provider{\n" +
+"    uint goodsAmount;\n" +
+"    uint profit;\n" +
+"    address addr;\n" +
+"}\n" +
+"\n" +
+"Provider[] providers;\n" +
+"uint provCount = 0;\n" +
+"uint totalPay;\n" +
+"address clientAddr;\n" +
+"event Deposit(address sender,address _provAddress, uint _value);\n" +
+"function init(address[] provAddrs, uint provCount,address mainAcc)\n" +
+"{\n" +
+"    clientAddr = mainAcc;\n" +
+"    for(uint i = 0;i<provCount;i++)\n" +
+"    {\n" +
+"        providers[i].profit = 0;\n" +
+"        providers[i].addr = provAddrs[0];\n" +
+"        provCount++;\n" +
+"    }\n" +
+"}\n" +
+"function pay(uint amount, address addr,uint goods)\n" +
+"{\n" +
+"    providers[search(addr)].profit += amount;\n" +
+"    providers[search(addr)].goodsAmount += goods;\n" +
+"    totalPay += amount;\n"  +
+"    Deposit(clientAddr,addr,amount);\n" +
+"}\n" +
+"function search(address tmpAddr) returns(uint)\n" +
+"{\n" +
+"    for(uint i = 0;i<provCount;i++)\n" +
+"    {\n" +
+"        if(providers[i].addr == tmpAddr) return i;\n" +
+"    }\n" +
+"}\n" +
+"function contractDone() payable\n" +
+"{\n" +
+"    for(uint i = 0;i<provCount;i++)\n" +
+"    {\n" +
+"      bool a = providers[i].addr.send(providers[i].profit);\n" +
+"    }\n" +
+"}\n" +
+"function contractNotDone() payable\n" +
+"{\n" +
+"  bool a = clientAddr.send(totalPay);\n" +
+"}\n" +
+"}\n" +;
 
       var compiled = web3.eth.compile.solidity(source);
       
-    
+
      $scope.testMethod = function(){
         console.log(compiled); 
         alert($scope.myFunds + "\n" + contract.address + "\n");
