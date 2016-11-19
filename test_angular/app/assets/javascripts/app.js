7cfed1ac2deb5bceb7cdb0c27538309368296939
Roman Anderson <greenclawanderson@gmail.com>
2016-11-19 20:55:03 +0300

    Contract generator added

--- test_angular/app/assets/javascripts/app.js
+++ test_angular/app/assets/javascripts/app.js
@@ -15,144 +15,102 @@ app.controller('smartDealsController',function($scope,$mdDialog){
 //geth --rpc --rpcapi admin,eth,personal,miner  --testnet --rpccorsdomain="*" console
 //----------------------------------------------------------------------------------------
     
-     var abi =  [
-     {"constant":false,
-      "inputs":
-        [{"name":"amount","type":"uint256"},
-         {"name":"addr","type":"address"},
-         {"name":"goods","type":"uint256"}],
-         "name":"pay",
-         "outputs":[],
-         "payable":false,
-         "type":"function"},
+     // var abi =  [{"constant":false,
+     //  "inputs":
+     //    [{"name":"amount","type":"uint256"},
+     //     {"name":"addr","type":"address"},
+     //     {"name":"goods","type":"uint256"}],
+     //     "name":"pay",
+     //     "outputs":[],
+     //     "payable":false,
+     //     "type":"function"},
 
-         {"constant":false,
-         "inputs":[],
-         "name":"contractDone",
-         "outputs":[],
-         "payable":true,
-         "type":"function"},
+     //     {"constant":false,
+     //     "inputs":[],
+     //     "name":"contractDone",
+     //     "outputs":[],
+     //     "payable":true,
+     //     "type":"function"},
 
-         {"constant":false,
-         "inputs":[],
-         "name":"contractNotDone",
-         "outputs":[],
-         "payable":true,
-         "type":"function"},
+     //     {"constant":false,
+     //     "inputs":[],
+     //     "name":"contractNotDone",
+     //     "outputs":[],
+     //     "payable":true,
+     //     "type":"function"},
 
-         {"constant":false,
-         "inputs":
-         [{"name":"tmpAddr",
-           "type":"address"}],
-           "name":"search",
-           "outputs":
-           [{"name":"",
-           "type":"uint256"}],
-           "payable":false,
-           "type":"function"},
+     //     {"constant":false,
+     //     "inputs":
+     //     [{"name":"tmpAddr",
+     //       "type":"address"}],
+     //       "name":"search",
+     //       "outputs":
+     //       [{"name":"",
+     //       "type":"uint256"}],
+     //       "payable":false,
+     //       "type":"function"},
 
-         {"constant":false,
-           "inputs":
-           [{"name":"provAddrs",
-           "type":"address[]"},
-           {"name":"provCount",
-           "type":"uint256"},
-           {"name":"mainAcc",
-           "type":"address"}],
-           "name":"init",
-           "outputs":[],
-           "payable":false,
-           "type":"function"},
+     //     {"constant":false,
+     //       "inputs":
+     //       [{"name":"provAddrs",
+     //       "type":"address[]"},
+     //       {"name":"provCount",
+     //       "type":"uint256"},
+     //       {"name":"mainAcc",
+     //       "type":"address"}],
+     //       "name":"init",
+     //       "outputs":[],
+     //       "payable":false,
+     //       "type":"function"},
 
-          {"anonymous":false,
-           "inputs":
-           [{"indexed":false,
-           "name":"sender",
-           "type":"address"},
-            {"indexed":false,
-            "name":"_provAddress",
-            "type":"address"},
-            {"indexed":false,
-            "name":"_value",
-            "type":"uint256"}],
-           "name":"Deposit",
-           "type":"event"}];
-     var Web3 = require('web3');
-     var web3 = new Web3();
-     var eth = web3.eth;
-     web3.setProvider(new web3.providers.HttpProvider("http://127.0.0.1:8545"));
-     var accounts = eth.accounts;
-     $scope.accounts = accounts;
-    //event Deposit(address sender,address _provAddress, uint _value);
-
-      
-
-     var contractAddress = '0x87a712ce7e30c848f78b1a4f0629f31b0e5f5db1';
-     var Myco = web3.eth.contract(abi);
-     var contract = Myco.at(contractAddress);
-     var filter = web3.eth.filter({fromBlock:0, toBlock: 'latest', address: contractAddress, 'topics':['0x' + 
-     web3.sha3('pay(uint256,uint256,uint256)')]});
+     //      {"anonymous":false,
+     //       "inputs":
+     //       [{"indexed":false,
+     //       "name":"sender",
+     //       "type":"address"},
+     //        {"indexed":false,
+     //        "name":"_provAddress",
+     //        "type":"address"},
+     //        {"indexed":false,
+     //        "name":"_value",
+     //        "type":"uint256"}],
+     //       "name":"Deposit",
+     //       "type":"event"}];
+     //event Deposit(address sender,address _provAddress, uint _value);
      
-     $scope.myToken = eth.coinbase;
-     $scope.myFunds = web3.fromWei(eth.getBalance(eth.coinbase),"ether");
-     web3.personal.unlockAccount($scope.myToken,"Dtrnjh2917");
-
-     var source = "pragma solidity ^0.4.0;\n"+ 
-"contract SmartDeal {\n" +
-"struct Provider{\n" +
-"    uint goodsAmount;\n" +
-"    uint profit;\n" +
-"    address addr;\n" +
-"}\n" +
-"\n" +
-"Provider[] providers;\n" +
-"uint provCount = 0;\n" +
-"uint totalPay;\n" +
-"address clientAddr;\n" +
-"event Deposit(address sender,address _provAddress, uint _value);\n" +
-"function init(address[] provAddrs, uint provCount,address mainAcc)\n" +
-"{\n" +
-"    clientAddr = mainAcc;\n" +
-"    for(uint i = 0;i<provCount;i++)\n" +
-"    {\n" +
-"        providers[i].profit = 0;\n" +
-"        providers[i].addr = provAddrs[0];\n" +
-"        provCount++;\n" +
-"    }\n" +
-"}\n" +
-"function pay(uint amount, address addr,uint goods)\n" +
-"{\n" +
-"    providers[search(addr)].profit += amount;\n" +
-"    providers[search(addr)].goodsAmount += goods;\n" +
-"    totalPay += amount;\n"  +
-"    Deposit(clientAddr,addr,amount);\n" +
-"}\n" +
-"function search(address tmpAddr) returns(uint)\n" +
-"{\n" +
-"    for(uint i = 0;i<provCount;i++)\n" +
-"    {\n" +
-"        if(providers[i].addr == tmpAddr) return i;\n" +
-"    }\n" +
-"}\n" +
-"function contractDone() payable\n" +
-"{\n" +
-"    for(uint i = 0;i<provCount;i++)\n" +
-"    {\n" +
-"      bool a = providers[i].addr.send(providers[i].profit);\n" +
-"    }\n" +
-"}\n" +
-"function contractNotDone() payable\n" +
-"{\n" +
-"  bool a = clientAddr.send(totalPay);\n" +
-"}\n" +
-"}\n" +;
-
-      var compiled = web3.eth.compile.solidity(source);
-      
 
      $scope.testMethod = function(){
-        console.log(compiled); 
         alert($scope.myFunds + "\n" + contract.address + "\n");
+     };
+
+     $scope.deployContract = function() {
+       var Web3 = require('web3');
+       var web3 = new Web3();
+       var eth = web3.eth;
+       web3.setProvider(new web3.providers.HttpProvider("http://127.0.0.1:8545"));
+       var accounts = eth.accounts;
+       var source = "contract Smart { function go(uint i) returns(uint) { return i*10; }}";
+       var compiled = eth.comile.solidity(source);
+       $scope.myToken = eth.coinbase;
+       web3.personal.unlockAccount($scope.myToken,"Dtrnjh2917");
+       var abi = compiled.Smart.info.abiDefinition;
+       var con = eth.contract(abi);
+       var smartContract = con.new({
+          from: accounts[0],
+          data: smartContract.Smart.code, gas: 2000000},
+          function(error, contract) {
+            if(!error) {
+              if(!contract.address){
+                console.log("Sended, please wait. Transaction: " +  contract.transactionHash);
+              } else {
+                console.log("Contract mined! Address: " + contract.address);
+                console.log(contract);
+              }
+            } else {
+              console.log(error);
+            }
+          }
+       });
      };
 
 //----------------------------------------------------------------------------------------
@@ -172,10 +130,24 @@ app.controller('smartDealsController',function($scope,$mdDialog){
       $mdDialog.hide(answer);
     };
 
-    $scope.initProviders = function(){
-      var a = 2;
-      var adr = ["0x9b423036f2324445ab650b4ef883d0d6ede53ca3", "0x75a3c71753c5E9f83c64a028849410f04cF85d95"];
-      contract.init(adr, a, $scope.myToken);
+           // var web3 = new Web3();
+     // var eth = web3.eth;
+     // web3.setProvider(new web3.providers.HttpProvider("http://127.0.0.1:8545"));
+     // var accounts = eth.accounts;
+     // web3.personal.unlockAccount(eth.coinbase,"Dtrnjh2917");
+     // var source = 'pragma solidity ^0.4.0; contract SmartDeal {  struct Provider{   uint goodsAmount;    uint profit;    address addr; } Provider[] providers; uint provCount = 0; uint totalPay; address clientAddr; event Deposit(address sender,address _provAddress, uint _value); function init(address[] provAddrs, uint provCount,address mainAcc) {clientAddr = mainAcc; for(uint i = 0;i<provCount;i++) {providers[i].profit = 0; providers[i].addr = provAddrs[0]; provCount++;}} function pay(uint amount, address addr,uint goods) { providers[search(addr)].profit += amount; providers[search(addr)].goodsAmount += goods; totalPay += amount; Deposit(clientAddr,addr,amount); } function search(address tmpAddr) returns(uint) { for(uint i = 0;i<provCount;i++) { if(providers[i].addr == tmpAddr) return i; }} function contractDone() payable { for(uint i = 0;i<provCount;i++) { bool a = providers[i].addr.send(providers[i].profit); }} function contractNotDone() payable {bool a = clientAddr.send(totalPay);}}'
+     // var compiled = web3.eth.compile.solidity(source);
+     //  var code = compiled.Multiply7.code;
+     //  var abi = compiled.Multiply7.info.abiDefinition;
+
+     //  web3.eth.contract(abi).new({from: "0xae4af6288b97b7e1ce9b3a343ed17aebe6e2c8f3", data: code}, function (err, contract) {
+     //  if (!err && contract.address)
+     //    console.log("deployed on:", contract.address);
+     //  });
+  
+
+    $scope.pay = function(){
+      contract.pay({"amount":1, "addr": 0x9b423036f2324445ab650b4ef883d0d6ede53ca3, "goods": 1})
     };
      $scope.contractDone = function(){
         contract.contractDone();
